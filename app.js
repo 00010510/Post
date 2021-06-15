1007dc40621a5aca55f567b32edb69f76ea1ded2
00010510wiut@gmail.com <00010510wiut@gmail.com>
2021-06-16 04:18:08 +0500

    adding style to index.pug

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const express = require('express')
+const app = express()
 
+app.set('view engine', 'pug')
+
+app.use('/static', express.static('public'))
+
+app.get('/',(req, res) => {
+    res.render('index')
+})
+app.listen(5000, err =>{
+    if(err) console.log(err)
+
+    console.log('Running')
+})
