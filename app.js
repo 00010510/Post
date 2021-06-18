54b47348d577e7fbdc9fad74bd1de673c6eb799e
00010510wiut@gmail.com <00010510wiut@gmail.com>
2021-06-19 04:24:13 +0500

    adding images, styling home and categories page

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ app.set('view engine', 'pug')
 
 app.use('/static', express.static('public'))
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.render('index')
 })
+
+app.get('/categories', (req, res) => {
+    res.render('categories')
+})
+
 app.listen(5000, err =>{
     if(err) console.log(err)
 
